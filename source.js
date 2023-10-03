9368ce221a6c023f7ecd5b743509d6d1e29fc69a
Francisco Javier Vadillo <32244264+FranVadillo@users.noreply.github.com>
2023-10-03 20:51:56 +0200

    Update source.js

--- source.js
+++ source.js
@@ -5,11 +5,13 @@ window.onload = function() {
     let newText, childs;
 console.log("Hello world!");
     for(let i = 0; i < texts.length; i++){
-        newText = entry.cloneNode(true);
-        childs = newText.childNodes;
-        childs[1].childNodes[0].innerHTML = texts[i];
-        childs[1].childNodes[0].href = "texts/"+texts[i]+".pdf"
-        row.appendChild(newText);
+        if (texts[i] != null) {
+            newText = entry.cloneNode(true);
+            childs = newText.childNodes;
+            childs[1].childNodes[0].innerHTML = texts[i];
+            childs[1].childNodes[0].href = "texts/"+texts[i]+".pdf"
+            row.appendChild(newText);
+        } 
     }
     entry.parentNode.removeChild(entry);
 };
