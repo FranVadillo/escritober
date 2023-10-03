95760edc1c3d39de2ee4d6cb81bbf6ddf619eadc
Francisco Javier Vadillo <32244264+FranVadillo@users.noreply.github.com>
2023-10-03 20:54:34 +0200

    Update source.js

--- source.js
+++ source.js
@@ -3,23 +3,12 @@ window.onload = function() {
     row = document.getElementById("row");
     entry = document.getElementById("entry");
     let newText, childs;
-console.log("Hello world!");
     for(let i = 0; i < texts.length; i++){
-        if (texts[i] != null) {
-            newText = entry.cloneNode(true);
-            childs = newText.childNodes;
-            childs[1].childNodes[0].innerHTML = texts[i];
-            childs[1].childNodes[0].href = "texts/"+texts[i]+".pdf"
-            row.appendChild(newText);
-        } 
+        newText = entry.cloneNode(true);
+        childs = newText.childNodes;
+        childs[1].childNodes[0].innerHTML = texts[i];
+        childs[1].childNodes[0].href = "texts/"+texts[i]+".pdf"
+        row.appendChild(newText);
     }
     entry.parentNode.removeChild(entry);
 };
-
-function r(){
-    console.log("cabezon")
-}
-
-function ImageError(_this){
-    _this.src = "images/missigno.jpg";
-}
