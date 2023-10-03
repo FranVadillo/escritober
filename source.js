2c7074fcde46b550822c5d1eaee7b9c24ae0805e
Francisco Javier Vadillo <32244264+FranVadillo@users.noreply.github.com>
2023-10-03 20:59:01 +0200

    Update source.js

--- source.js
+++ source.js
@@ -3,7 +3,7 @@ window.onload = function() {
     row = document.getElementById("row");
     entry = document.getElementById("entry");
     let newText, childs;
-    for(let i = 0; i < texts.length; i++){
+    for(let i = 0; i < 3; i++){
         newText = entry.cloneNode(true);
         childs = newText.childNodes;
         childs[1].childNodes[0].innerHTML = texts[i];
