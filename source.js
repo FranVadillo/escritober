721146cc85136160420198a5d3c3419b11b0163e
Francisco Javier Vadillo <32244264+FranVadillo@users.noreply.github.com>
2024-10-02 19:21:35 +0200

    Update source.js

--- source.js
+++ source.js
@@ -1,17 +1,23 @@
 window.onload = function() {
     let row;
     row = document.getElementById("row");
-    entry = document.getElementById("entry");
+    entry = document.getElementById("entry-2023");
     let newText, childs;
-    for (let year = 2023; year < 2025; year++){
-        
-        for(let i = 0; i < 31; i++){
-            newText = entry.cloneNode(true);
-            childs = newText.childNodes;
-            childs[1].childNodes[0].innerHTML = texts[i];
-            childs[1].childNodes[0].href = "texts/"+texts[i]+".pdf"
-            row.appendChild(newText);
-        }
+    for(let i = 0; i < 31; i++){
+        newText = entry.cloneNode(true);
+        childs = newText.childNodes;
+        childs[1].childNodes[0].innerHTML = texts[i];
+        childs[1].childNodes[0].href = "texts/"+texts[i]+".pdf"
+        row.appendChild(newText);
+    }
+    entry.parentNode.removeChild(entry);
+    entry = document.getElementById("entry-2024");
+    for(let i = 0; i < 31; i++){
+        newText = entry.cloneNode(true);
+        childs = newText.childNodes;
+        childs[1].childNodes[0].innerHTML = texts[i];
+        childs[1].childNodes[0].href = "texts/"+texts[i]+".pdf"
+        row.appendChild(newText);
     }
     entry.parentNode.removeChild(entry);
 };
