c5dfd6723dba13107dc92a5486ddb727e53c97af
Francisco Javier Vadillo <32244264+FranVadillo@users.noreply.github.com>
2024-10-02 19:28:04 +0200

    Update source.js

--- source.js
+++ source.js
@@ -6,8 +6,8 @@ window.onload = function() {
     for(let i = 0; i < 31; i++){
         newText = entry.cloneNode(true);
         childs = newText.childNodes;
-        childs[1].childNodes[0].innerHTML = texts[i];
-        childs[1].childNodes[0].href = "texts/"+texts[i]+".pdf"
+        childs[1].childNodes[0].innerHTML = texts2023[i];
+        childs[1].childNodes[0].href = "texts-2023/"+texts2023[i]+".pdf"
         row.appendChild(newText);
     }
     row = document.getElementById("row-2024");
@@ -16,8 +16,8 @@ window.onload = function() {
     for(let i = 0; i < 31; i++){
         newText = entry.cloneNode(true);
         childs = newText.childNodes;
-        childs[1].childNodes[0].innerHTML = texts[i];
-        childs[1].childNodes[0].href = "texts/"+texts[i]+".pdf"
+        childs[1].childNodes[0].innerHTML = texts2024[i];
+        childs[1].childNodes[0].href = "texts-2024/"+texts2024[i]+".pdf"
         row.appendChild(newText);
     }
     entry.parentNode.removeChild(entry);
