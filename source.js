e65c7fa339cc4ade72780d2c23929c2300c33270
Francisco Javier Vadillo <32244264+FranVadillo@users.noreply.github.com>
2024-10-02 19:19:18 +0200

    Update source.js

--- source.js
+++ source.js
@@ -3,13 +3,15 @@ window.onload = function() {
     row = document.getElementById("row");
     entry = document.getElementById("entry");
     let newText, childs;
-    const d = new Date();
-    for(let i = 0; i < 31; i++){
-        newText = entry.cloneNode(true);
-        childs = newText.childNodes;
-        childs[1].childNodes[0].innerHTML = texts[i];
-        childs[1].childNodes[0].href = "texts/"+texts[i]+".pdf"
-        row.appendChild(newText);
+    for (let year = 2023; year < 2025; year++){
+        
+        for(let i = 0; i < 31; i++){
+            newText = entry.cloneNode(true);
+            childs = newText.childNodes;
+            childs[1].childNodes[0].innerHTML = texts[i];
+            childs[1].childNodes[0].href = "texts/"+texts[i]+".pdf"
+            row.appendChild(newText);
+        }
     }
     entry.parentNode.removeChild(entry);
 };
